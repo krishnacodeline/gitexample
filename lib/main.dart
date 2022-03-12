e8a17b7a5177656e4d7be3cfca0112241591980c
krishna <krishnacodeline@gmail.com>
2022-03-12 16:59:58 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  print();
+  //print();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
